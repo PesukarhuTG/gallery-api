0a67b24babbbf4bbb6ad6b78cd9e7b161e540faf
Tatiana <ganzhinovat@yandex.ru>
2022-02-09 16:10:57 +0300

    feat: implement add images on page

---
+++ modules/renderCards.js
@@ -0,0 +1,23 @@
+const listCards = document.querySelector('.grid');
+
+const renderCards = async (data) => {
+  listCards.textContent = '';
+
+  const cards = data.map(item => {
+
+    const card = document.createElement('div');
+    card.className = `item ${(item.width > item.height) ? 'item-horizontal' : 'item-vertical'}`;
+    card.style.backgroundImage = `url('${item.links.download}')`;
+
+    const p = document.createElement('p');
+    p.className = 'item-size';
+    p.textContent = `${item.width}x${item.height}`
+
+    card.append(p);
+    return card;
+  });
+
+  listCards.append(...cards);
+};
+
+export default renderCards;
--- modules/renderPhoto.js
+++ modules/renderPhoto.js
@@ -1,13 +1,18 @@
 import { getPhotos } from "./service.js";
+import renderCards from "./renderCards.js";
+
 const headerPoster = document.querySelector('.header');
 
 const firstRender = data => {
   headerPoster.style.backgroundImage = `url('${data.links.download}')`;
-}
+};
 
 const renderPhoto = async () => {
   const data = await getPhotos();
-  firstRender(data[0]);
-}
+  const [firstImage, ...otherImages] = data;
+
+  firstRender(firstImage);
+  renderCards(otherImages);
+};
 
 export default renderPhoto;
