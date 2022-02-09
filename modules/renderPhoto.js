9048df558e74eef76e27d823b9cea5cdb91ea47f
Tatiana <ganzhinovat@yandex.ru>
2022-02-09 11:48:34 +0300

    feat: implement change header bgr from API data

--- modules/renderPhoto.js
+++ modules/renderPhoto.js
@@ -1,7 +1,13 @@
 import { getPhotos } from "./service.js";
+const headerPoster = document.querySelector('.header');
 
-const renderPhoto = () => {
-  getPhotos();
+const firstRender = data => {
+  headerPoster.style.backgroundImage = `url('${data.links.download}')`;
+}
+
+const renderPhoto = async () => {
+  const data = await getPhotos();
+  firstRender(data[0]);
 }
 
 export default renderPhoto;
